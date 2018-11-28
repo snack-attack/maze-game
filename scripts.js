a25c18e95dec720d06a47741b461ced114109282
Audrey Hayes <audreyhayes0@gmail.com>
2018-11-28 17:35:31 +0000

    bg rect

--- scripts.js
+++ scripts.js
@@ -1,53 +1,12 @@
 ((d) => {
     let app = d.getElementById('root');
-    let svg = d.createElement('svg');
-    let rect = d.createElement('rect');
-    let button = d.createElement('button');
+    let canvas = d.createElement('canvas');
+    let ctx = canvas.getContext('2d');
 
-    app.classList.add('container');
-    app.appendChild(svg);
-    svg.appendChild(rect);
-    app.appendChild(button);
+    app.appendChild(canvas);
 
-    
-    svg.setAttribute('width', '500');
-    svg.setAttribute('height', '500');
-
-    button.textContent = 'Play!';
-
-    rect.setAttribute('x', '0');
-    rect.setAttribute('y', '0');
-    rect.setAttribute('width', '500');
-    rect.setAttribute('height', '500');
-    rect.classList.add('bg');
-
-
-    //create a rectangle and set x, y, width, height
-    let rectFactory = (xcord, ycord, w, h) => {
-        let rect = d.createElement('rect');
-        svg.appendChild(rect);
-        rect.classList.add('path');
-
-        //set x-cord
-        rect.setAttribute('x', 'xcord');
-
-        //set y-cord
-        rect.setAttribute('y', 'xcord');
-
-        //set width
-        rect.setAttribute('width', 'w');
-
-        //set height
-        rect.setAttribute('height', 'h');
-
-    }
-
-    //create a test rect
-
-    rectFactory(0, 50, 290, 30);
-
-
-
+    ctx.rect(20,20,150,100);
+    ctx.stroke();
 
 
 })(document);
