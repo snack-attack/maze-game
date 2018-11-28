86bd44f7276ada56aea837e6a0b859ce34db0e24
Audrey Hayes <audreyhayes0@gmail.com>
2018-11-28 17:14:52 +0000

    created rect factory function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 ((d) => {
     let app = d.getElementById('root');
-    let container = d.createElement('div');
     let svg = d.createElement('svg');
     let rect = d.createElement('rect');
+    let button = d.createElement('button');
 
-    container.classList.add('container');
-    
-    
-    app.appendChild(container);
-    container.appendChild(svg);
+    app.classList.add('container');
+    app.appendChild(svg);
     svg.appendChild(rect);
+    app.appendChild(button);
 
     
     svg.setAttribute('width', '500');
     svg.setAttribute('height', '500');
+
+    button.textContent = 'Play!';
 
     rect.setAttribute('x', '0');
     rect.setAttribute('y', '0');
@@ -21,17 +21,32 @@
     rect.setAttribute('height', '500');
     rect.classList.add('bg');
 
-    let rectFactory = (xcord, y, width, height) => {
+
+    //create a rectangle and set x, y, width, height
+    let rectFactory = (xcord, ycord, w, h) => {
+        let rect = d.createElement('rect');
+        svg.appendChild(rect);
+        rect.classList.add('path');
+
         //set x-cord
         rect.setAttribute('x', 'xcord');
 
         //set y-cord
+        rect.setAttribute('y', 'xcord');
 
         //set width
+        rect.setAttribute('width', 'w');
 
         //set height
+        rect.setAttribute('height', 'h');
 
     }
+
+    //create a test rect
+
+    rectFactory(0, 50, 290, 30);
+
+
 
 
 
